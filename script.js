d5ce4374796f0a6bdbfc7e16d841b0e284a42c71
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-23 11:45:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the dropdown list and the button
+  const colorSelect = document.getElementById("colorSelect");
+  const removeButton = document.querySelector('input[type="button"]');
 
-const removeButton = document.querySelector('input[type="button"]');
+  // Add a click event listener to the button
+  removeButton.addEventListener("click", function () {
+    // Get the selected index of the dropdown list
+    const selectedIndex = colorSelect.selectedIndex;
 
-const selectColor = document.getElementById("colorSelect");
-
-
-removeButton.addEventListener("click",()=>{
-	const idx = selectColor.seletedIndex;
-
-	if(idx !== -1)
-	{
-		selectColor.remove(idx);
-	}
+    // Check if any option is selected
+    if (selectedIndex !== -1) {
+      // Remove the selected option from the dropdown list
+      colorSelect.remove(selectedIndex);
+    }
+  });
 });

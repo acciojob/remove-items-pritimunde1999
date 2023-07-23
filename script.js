00ad32180395b9b087975f4ae686b0f398daefae
Priti Munde <120581202+pritimunde1999@users.noreply.github.com>
2023-07-23 11:37:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+const removeButton = document.querySelector('input[type="button"]');
+
+const selectColor = document.getElementById("colorSelect");
+
+
+removeButton.addEventListener("click",()=>{
+	const idx = selectColor.seletedIndex;
+
+	if(idx !== -1)
+	{
+		selectColor.remove(idx);
+	}
+});
